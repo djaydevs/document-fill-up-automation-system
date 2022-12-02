4222945599b84f112f68702b14f7ffee0ce4cfe9
Danny Jay Flores <80023967+djaydevs@users.noreply.github.com>
2022-12-02 21:16:59 +0800

    update login form design

--- src/newpackage/LogInForm.java
+++ src/newpackage/LogInForm.java
@@ -15,6 +15,7 @@ public class LogInForm extends javax.swing.JFrame {
      */
     public LogInForm() {
         initComponents();
+        
     }
 
     /**
@@ -26,21 +27,127 @@ public class LogInForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        lbforgotpass = new javax.swing.JLabel();
+        btnlogin = new com.k33ptoo.components.KButton();
+        etpass = new javax.swing.JTextField();
+        etuname = new javax.swing.JTextField();
+        line = new javax.swing.JLabel();
+        line1 = new javax.swing.JLabel();
+        icpass = new javax.swing.JLabel();
+        lbpass = new javax.swing.JLabel();
+        icuname = new javax.swing.JLabel();
+        lbuname = new javax.swing.JLabel();
+        lblogin = new javax.swing.JLabel();
+        lbtitle = new javax.swing.JLabel();
+        lbbrgy = new javax.swing.JLabel();
+        logo = new javax.swing.JLabel();
+        loginbg = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lbforgotpass.setFont(new java.awt.Font("Microsoft YaHei", 0, 30)); // NOI18N
+        lbforgotpass.setForeground(new java.awt.Color(104, 185, 225));
+        lbforgotpass.setText("<html><p><u>Forgot Password?</u></p></html>");
+        jPanel1.add(lbforgotpass, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 850, -1, -1));
+
+        btnlogin.setText("LOG IN");
+        btnlogin.setFont(new java.awt.Font("Microsoft YaHei", 1, 30)); // NOI18N
+        btnlogin.setkBackGroundColor(new java.awt.Color(255, 255, 255));
+        btnlogin.setkBorderRadius(70);
+        btnlogin.setkEndColor(new java.awt.Color(104, 185, 225));
+        btnlogin.setkHoverEndColor(new java.awt.Color(13, 76, 146));
+        btnlogin.setkHoverForeGround(new java.awt.Color(255, 255, 255));
+        btnlogin.setkHoverStartColor(new java.awt.Color(13, 76, 146));
+        btnlogin.setkSelectedColor(new java.awt.Color(255, 255, 255));
+        btnlogin.setkStartColor(new java.awt.Color(13, 76, 146));
+        btnlogin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnloginActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnlogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(1134, 624, 237, 73));
+
+        etpass.setFont(new java.awt.Font("Microsoft YaHei", 0, 30)); // NOI18N
+        etpass.setBorder(null);
+        jPanel1.add(etpass, new org.netbeans.lib.awtextra.AbsoluteConstraints(870, 510, 500, 70));
+
+        etuname.setFont(new java.awt.Font("Microsoft YaHei", 0, 30)); // NOI18N
+        etuname.setBorder(null);
+        etuname.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                etunameActionPerformed(evt);
+            }
+        });
+        jPanel1.add(etuname, new org.netbeans.lib.awtextra.AbsoluteConstraints(870, 325, 500, 70));
+
+        line.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/loginline.png"))); // NOI18N
+        jPanel1.add(line, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 396, -1, -1));
+
+        line1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/loginline.png"))); // NOI18N
+        jPanel1.add(line1, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 580, -1, -1));
+
+        icpass.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/iconlock.png"))); // NOI18N
+        jPanel1.add(icpass, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 495, -1, -1));
+
+        lbpass.setFont(new java.awt.Font("Microsoft YaHei", 0, 30)); // NOI18N
+        lbpass.setForeground(new java.awt.Color(3, 87, 122));
+        lbpass.setText("Password");
+        jPanel1.add(lbpass, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 447, -1, -1));
+
+        icuname.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/iconperson.png"))); // NOI18N
+        jPanel1.add(icuname, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 316, -1, -1));
+
+        lbuname.setFont(new java.awt.Font("Microsoft YaHei", 0, 30)); // NOI18N
+        lbuname.setForeground(new java.awt.Color(3, 87, 122));
+        lbuname.setText("Username");
+        jPanel1.add(lbuname, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 274, -1, -1));
+
+        lblogin.setFont(new java.awt.Font("Microsoft YaHei", 1, 48)); // NOI18N
+        lblogin.setForeground(new java.awt.Color(3, 87, 122));
+        lblogin.setText("Log In");
+        jPanel1.add(lblogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(785, 177, -1, -1));
+
+        lbtitle.setFont(new java.awt.Font("Microsoft YaHei", 1, 48)); // NOI18N
+        lbtitle.setForeground(new java.awt.Color(12, 57, 107));
+        lbtitle.setText("<html><p><center>DOCUMENT FILL-UP<br>AUTOMATION SYSTEM</center></p></html>");
+        jPanel1.add(lbtitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(75, 430, -1, -1));
+
+        lbbrgy.setFont(new java.awt.Font("Microsoft YaHei", 1, 40)); // NOI18N
+        lbbrgy.setForeground(new java.awt.Color(255, 255, 255));
+        lbbrgy.setText("BARANGAY FATIMA 1");
+        jPanel1.add(lbbrgy, new org.netbeans.lib.awtextra.AbsoluteConstraints(136, 300, -1, -1));
+
+        logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/loginlogo.png"))); // NOI18N
+        jPanel1.add(logo, new org.netbeans.lib.awtextra.AbsoluteConstraints(255, 76, -1, -1));
+
+        loginbg.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/loginbg.png"))); // NOI18N
+        jPanel1.add(loginbg, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 1440, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnloginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnloginActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnloginActionPerformed
+
+    private void etunameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_etunameActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_etunameActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +185,21 @@ public class LogInForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.k33ptoo.components.KButton btnlogin;
+    private javax.swing.JTextField etpass;
+    private javax.swing.JTextField etuname;
+    private javax.swing.JLabel icpass;
+    private javax.swing.JLabel icuname;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lbbrgy;
+    private javax.swing.JLabel lbforgotpass;
+    private javax.swing.JLabel lblogin;
+    private javax.swing.JLabel lbpass;
+    private javax.swing.JLabel lbtitle;
+    private javax.swing.JLabel lbuname;
+    private javax.swing.JLabel line;
+    private javax.swing.JLabel line1;
+    private javax.swing.JLabel loginbg;
+    private javax.swing.JLabel logo;
     // End of variables declaration//GEN-END:variables
 }
